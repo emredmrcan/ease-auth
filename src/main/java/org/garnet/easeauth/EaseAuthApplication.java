4abc6d3d1614251789305fa17a9cee1f0c4a3fd8
emredemircan <emre.demircan@ceng.deu.edu.tr>
2022-06-12 12:54:44 +0200

    init commit

---
+++ src/main/java/org/garnet/easeauth/EaseAuthApplication.java
@@ -0,0 +1,13 @@
+package org.garnet.easeauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/garnet/easeauth/EaseAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.garnet.easeauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
